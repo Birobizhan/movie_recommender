1a8ff68887333226954a57c65fc53ea0ebc665f5
Birobizhan <fedorovd2005@gmail.ru>
2025-05-05 16:53:43 +0300

    Добавил логгирование и еще фильмов

--- list_films/create_films_review.py
+++ list_films/create_films_review.py
@@ -1,11 +1,13 @@
 from environs import Env
 from openai import OpenAI
 import time
+import logging
 import re
 from openai.types.chat import ChatCompletion
 from typing import Callable, TypeVar
 
-# Аннотация типа для возвращаемого значения функции
+logging.basicConfig(level=logging.INFO, filename="create_films_review_log.log", filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
 T = TypeVar('T')
 
 
@@ -17,13 +19,26 @@ class Retry:
 
     def __call__(self, func: Callable[..., T]) -> Callable[..., T]:
         def wrapper(*args, **kwargs) -> T:
-            last_exception = None
             for attempt in range(self.max_retries):
                 try:
                     return func(*args, **kwargs)
+
+                except TypeError as error:
+                    logging.warning('Ошибка типов, скорее всего закончились попытки на токене', exc_info=True)
+                    print(f"Попытка {attempt + 1} из {self.max_retries} завершилась с ошибкой: {error}")
+
+                except IndexError as error:
+                    logging.error('Ошибка индексирования, пришел не тот ответ', exc_info=True)
+                    print(f"Попытка {attempt + 1} из {self.max_retries} завершилась с ошибкой: {error}")
+
+                except ValueError as error:
+                    logging.error(f'{error}, скорее всего не совпадают названия фильмов на запросе и ответе', exc_info=True)
+                    print(f"Попытка {attempt + 1} из {self.max_retries} завершилась с ошибкой: {error}")
+
                 except Exception as e:
-                    last_exception = e
+                    logging.error(e, exc_info=True)
                     print(f"Попытка {attempt + 1} из {self.max_retries} завершилась с ошибкой: {e}")
+
             raise Exception(f"Все {self.max_retries} попыток завершились ошибкой")
         return wrapper
 
@@ -49,17 +64,18 @@ def swap_title(title_film: str) -> str:
         content[0] = ''.join(part)
     with open('prompt1.txt', 'w', encoding='utf-8') as file_prompt_write:
         file_prompt_write.writelines(content)
+    logging.info(f'Изменено название в файле с промптом на {title_film}')
     return part[1][2:-1].rstrip()
 
 
 def add_to_csv_file(result, part_title, information) -> bool:
-    print(result[0] == part_title)
+    logging.info(f'Запрос и ответ одинаковые: {result[0] == part_title}')
     if result[0] == part_title:
         with open(f'genre_csv/{result[1]}.csv', 'a', encoding='utf-8') as file_add:
             file_add.write(f"\n{information}")
         with open('count.txt', 'a', encoding='utf-8') as file_add:
             file_add.write(f'{part_title}\n')
-        print(part_title, i)
+        logging.info(f'Удачно добавлен фильм {part_title}, в этом цикле он {i+1}')
         return True
     else:
         return False
@@ -73,9 +89,8 @@ def process(film) -> list[str]:
     answer = client_user.prompt(request_prompt)
     information_ans = answer.choices[0].message.content.split('\n')[1]
     res = re.split(r',(?=\S)', information_ans)
-    print(res)
-    print(part_title_swap)
-    print(information_ans)
+    logging.info(f'Ответ на промпт: {information_ans}')
+    logging.info(f'Фильм {res}')
     responds = add_to_csv_file(res, part_title_swap, information_ans)
     if responds is False:
         raise ValueError('Не совпадают названия на входе и выходе')
@@ -93,5 +108,5 @@ with open('right_top1000.txt', 'r', encoding='utf-8') as file:
     films_list = file.readlines()
 for i in range(len(films_list)):
     resulting = process(films_list[i])
-    print(resulting, i)
+    logging.info(f'Итерация с фильмом {resulting} удачно завершилась')
     time.sleep(2)
